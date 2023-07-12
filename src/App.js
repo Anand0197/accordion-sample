46694d7df48dab11973c1a0d87c7ea5bbe9336af
Anand <aanand.raj.patna@gmail.com>
2023-07-12 21:18:53 +0530

    commit to make one accordion open at a time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const faqs = [
+  {
+    title: 'Where are these chairs assembled?',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+  },
+  {
+    title: 'How long do I have to return my chair?',
+    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+  },
+  {
+    title: 'Do you ship to countries outside the EU?',
+    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+  },
+];
+
+export default function App() {
+  return <Accordion />;
+}
+
+function Accordion() {
+  const [token, setToken] = useState(null);
+  function handleToggle(id) {
+    token !== id ? setToken(id) : setToken(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="accordion">
+      {faqs.map((ques, i) => (
+        <Card
+          title={ques.title}
+          ans={ques.text}
+          key={i}
+          id={i}
+          token={token}
+          onToggle={handleToggle}
+        />
+      ))}
     </div>
   );
 }
 
-export default App;
+function Card({ title, ans, ques, id, token, onToggle }) {
+  // const [isOpen, setIsOpen] = useState(false);
+  // function handleToggle() {
+  //   setIsOpen(!isOpen);
+  // }
+  return (
+    <div className="card" onClick={() => onToggle(id + 1)}>
+      <p>{id <= 9 ? `0${id + 1}` : `${id + 1}`}</p>
+      <p>{title}</p>
+      <p>{token === id + 1 ? '-' : '+'}</p>
+      {token === id + 1 ? <div className="content">{ans}</div> : ''}
+    </div>
+  );
+}
